17e43bccad7e15d06f176166dc4a7ec924aadc65
michelhora <33845075+michelhora@users.noreply.github.com>
2019-12-03 10:14:03 -0300

    Add files via upload

---
+++ desafio-01/app.js
@@ -0,0 +1,11 @@
+new Vue
+    ({
+        el: '#desafio',
+
+        data: 
+    {
+        nome: Michel,
+        idade: 25,
+        imagem: 'http://files.cod3r.com.br/curso-vue/vue.jpg'
+    }
+})
